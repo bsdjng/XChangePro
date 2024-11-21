857ec6763bf26b0641ad7d0a23587fce19463a19
bsdjng <49759233+bsdjng@users.noreply.github.com>
2024-11-21 12:42:41 +0100

    graph en api basis

--- scripts/loadNavbar.js
+++ scripts/loadNavbar.js
@@ -1,15 +1,14 @@
 // Script om de navbar in te laden
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("views/navbar.html")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.text();
-      })
-      .then((data) => {
-        document.getElementById("navbar-container").innerHTML = data;
-      })
-      .catch((error) => console.error("Error loading navbar:", error));
-  });
-  
+  fetch("views/navbar.html")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then((data) => {
+      document.getElementById("navbar-container").innerHTML = data;
+    })
+    .catch((error) => console.error("Error loading navbar:", error));
+});
